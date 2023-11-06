7e1028d393e1a929bd9c491ff9c7d41118f07e06
Valeria Cerutti <97165687+vcerut@users.noreply.github.com>
2023-11-06 14:39:14 +0100

    configurazione tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ShowDetailsComponent } from './components/show-details/show-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ShowDetailsComponent,
   ],
   imports: [
     BrowserModule,
